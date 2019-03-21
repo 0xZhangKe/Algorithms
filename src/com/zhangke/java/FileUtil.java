a3936656bda612bf8f78d099f0c8915f23711f3c
ZhangKe <zhangke_711@163.com>
2019-03-21 22:39:15 +0800

    添加文件操作工具类

---
+++ src/com/zhangke/java/FileUtil.java
@@ -0,0 +1,39 @@
+package com.zhangke.java;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.util.Random;
+
+/**
+ * 文件工具类
+ * <p>
+ * Created by ZhangKe on 2019/3/21.
+ */
+public class FileUtil {
+
+    /**
+     * 创建一个随机数文件
+     */
+    public static void createRandomIntFile(File file, int maxNumber, int size) {
+        if (!file.exists()) {
+            try {
+                if (!file.createNewFile()) {
+                    return;
+                }
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+                return;
+            }
+        }
+        Random random = new Random();
+        try (FileWriter fileWriter = new FileWriter(file);
+             BufferedWriter bw = new BufferedWriter(fileWriter)) {
+            for (int i = 0; i < size; i++) {
+                bw.write(random.nextInt(maxNumber) + "\n");
+            }
+        } catch (Throwable e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
