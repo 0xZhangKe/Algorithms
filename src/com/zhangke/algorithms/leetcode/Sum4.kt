8292791ebf77cfb59ab43b4e60a516fe8ab2781f
ZhangKe <zhangke@qingting.fm>
2019-09-05 10:03:35 +0800

    添加 四数之和

---
+++ src/com/zhangke/algorithms/leetcode/Sum4.kt
@@ -0,0 +1,24 @@
+package com.zhangke.algorithms.leetcode
+
+/**
+ * 18.四数之和：https://leetcode-cn.com/problems/4sum/
+ *
+ * Created by ZhangKe on 2019/9/5.
+ */
+class Sum4 {
+
+    class Solution {
+        fun fourSum(nums: IntArray, target: Int): List<List<Int>> {
+
+        }
+    }
+}
+
+fun main(args: Array<String>) {
+    val nums = IntArray(5)
+    nums[0] = -1
+    nums[1] = 0
+    nums[2] = 1
+    nums[3] = 1
+    nums[4] = 55
+}
