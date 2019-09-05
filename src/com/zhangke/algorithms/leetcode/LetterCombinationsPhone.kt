7eb9d6061cc461c1b19b3b230cd24f04482c3ed2
ZhangKe <zhangke@qingting.fm>
2019-09-05 09:56:52 +0800

    完成电话号码的字母组合

--- src/com/zhangke/algorithms/leetcode/LetterCombinationsPhone.kt
+++ src/com/zhangke/algorithms/leetcode/LetterCombinationsPhone.kt
@@ -13,48 +13,10 @@ import kotlin.collections.HashMap
  */
 class LetterCombinationsPhone {
 
-//    data class TreeNode(val value: Char, var childs: List<TreeNode>? = null, var parent: TreeNode? = null)
-
     class Solution {
-//        fun letterCombinations(digits: String): List<String> {
-//            val map = HashMap<Char, CharArray>(8).apply {
-//                put('2', charArrayOf('a', 'b', 'c'))
-//                put('3', charArrayOf('d', 'e', 'f'))
-//                put('4', charArrayOf('g', 'h', 'i'))
-//                put('5', charArrayOf('j', 'k', 'l'))
-//                put('6', charArrayOf('m', 'n', 'o'))
-//                put('7', charArrayOf('p', 'q', 'r', 's'))
-//                put('8', charArrayOf('t', 'u', 'v'))
-//                put('9', charArrayOf('w', 'x', 'y', 'z'))
-//            }
-//            val digitArray = digits.toCharArray()
-//            var treeNode: TreeNode? = null
-//            var curTreeNodes: List<TreeNode>? = null
-//            var arrayTmp: CharArray
-//            for (i in digitArray.indices) {
-//                arrayTmp = map.get(digitArray[i])!!
-//                val list = mutableListOf<TreeNode>()
-//                for (c in arrayTmp) {
-//                    list.add(TreeNode(c))
-//                }
-//                if (i == 0) {
-//                    treeNode = TreeNode('-', list)
-//                } else {
-//                    for (node in curTreeNodes!!) {
-//                        for (n in list) {
-//                            n.parent = node
-//                        }
-//                        node.childs = list
-//                    }
-//                }
-//                curTreeNodes = list
-//            }
-//            val list = ArrayList<String>()
-//            dfs(treeNode!!, list)
-//            return list
-//        }
 
         fun letterCombinations(digits: String): List<String> {
+            if (digits.isEmpty()) return listOf()
             val map = HashMap<Char, CharArray>(8).apply {
                 put('2', charArrayOf('a', 'b', 'c'))
                 put('3', charArrayOf('d', 'e', 'f'))
@@ -71,30 +33,18 @@ class LetterCombinationsPhone {
                 array[i] = map[digitArray[i]]!!
             }
             val list = ArrayList<String>()
-            dfss(array, 0, 0, list, Stack<Char>())
+            dfs(array, -1, 0, list, java.util.Stack<Char>())
             return list
         }
 
-//        fun dfs(treeNode: TreeNode, list: ArrayList<String>) {
-//            if (treeNode.childs.isNullOrEmpty()) {
-//                val builder = StringBuilder()
-//                getTreeNodeValues(treeNode, builder)
-//                list.add(builder.toString())
-//            } else {
-//                for (n in treeNode.childs!!) {
-//                    dfs(n, list)
-//                }
-//            }
-//        }
-
-        fun dfss(array: Array<CharArray?>, firstPositon: Int,
-                 secondPosition: Int, list: ArrayList<String>,
-                 stack: Stack<Char>) {
-            if (array.size <= firstPositon || array[firstPositon]!!.size <= secondPosition) return
-            println("first:$firstPositon, second:$secondPosition")
-            stack.push(array[firstPositon]!![secondPosition])
+        fun dfs(array: Array<CharArray?>, firstPositon: Int,
+                secondPosition: Int, list: ArrayList<String>,
+                stack: java.util.Stack<Char>) {
+            if (array.size <= firstPositon || (firstPositon >= 0 && array[firstPositon]!!.size <= secondPosition)) return
+            if (firstPositon >= 0) {
+                stack.push(array[firstPositon]!![secondPosition])
+            }
             if (array.size <= firstPositon + 1) {
-                //换行
                 list.add(StringBuilder().apply {
                     val li = ArrayList<Char>()
                     val iterator = stack.iterator()
@@ -105,22 +55,15 @@ class LetterCombinationsPhone {
                         append(c)
                     }
                 }.toString())
-            } else if (array[firstPositon]!!.size > secondPosition + 1) {
-                for (i in secondPosition..4) {
-                    dfss(array, firstPositon + 1, i, list, stack)
+            } else {
+                for (i in 0..4) {
+                    dfs(array, firstPositon + 1, i, list, stack)
                 }
             }
-            try {
+            if (!stack.isEmpty()) {
                 stack.pop()
-            } catch (e: Exception) {
-                //ignore
             }
         }
-
-//        fun getTreeNodeValues(treeNode: TreeNode, builder: StringBuilder) {
-//            builder.append(treeNode.value)
-//            treeNode.parent?.let { getTreeNodeValues(it, builder) }
-//        }
     }
 }
 
