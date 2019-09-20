d94c3b9b563feee5283e32690bd5e2b74082a8e8
ZhangKe <zhangke@qingting.fm>
2019-09-20 17:00:42 +0800

    添加 括号生成

--- src/com/zhangke/algorithms/leetcode/GenerateParentheses.java
+++ src/com/zhangke/algorithms/leetcode/GenerateParentheses.java
@@ -1,6 +1,7 @@
 package com.zhangke.algorithms.leetcode;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -11,12 +12,31 @@ import java.util.List;
 public class GenerateParentheses {
 
     public List<String> generateParenthesis(int n) {
-        List<Character> charList = new ArrayList<>();
-        for(int i = 0; i < n * 2; i++){
-            charList.add(i % 2 == 0 ? '(' : ')');
+        LinkedList<LinkedList<String>> result = new LinkedList<LinkedList<String>>();
+        if (n == 0)
+            return result.get(0);
+        LinkedList<String> list0 = new LinkedList<String>();
+        list0.add("");
+        result.add(list0);
+        LinkedList<String> list1 = new LinkedList<String>();
+        list1.add("()");
+        result.add(list1);
+        for (int i = 2; i <= n; i++) {
+            LinkedList<String> temp = new LinkedList<String>();
+            for (int j = 0; j < i; j++) {
+                List<String> str1 = result.get(j);
+                List<String> str2 = result.get(i - 1 - j);
+                for (String s1 : str1) {
+                    for (String s2 : str2) {
+                        String el = "(" + s1 + ")" + s2;
+                        temp.add(el);
+                    }
+                }
+
+            }
+            result.add(temp);
         }
-        List<String> result = new ArrayList<>();
-        for()
+        return result.get(n);
     }
 
     public static void main(String[] args) {
