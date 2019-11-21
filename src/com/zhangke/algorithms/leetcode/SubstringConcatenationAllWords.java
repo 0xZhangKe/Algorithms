55a9c4e04e2bfad89123ff817021c9e35c4b3170
ZhangKe <zhangke@qingting.fm>
2019-11-21 11:10:19 +0800

    修改 串联所有单词的子串

--- src/com/zhangke/algorithms/leetcode/SubstringConcatenationAllWords.java
+++ src/com/zhangke/algorithms/leetcode/SubstringConcatenationAllWords.java
@@ -1,7 +1,9 @@
 package com.zhangke.algorithms.leetcode;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Stack;
 
 /**
  * 30. 串联所有单词的子串:
@@ -11,16 +13,37 @@ import java.util.List;
 public class SubstringConcatenationAllWords {
 
     public List<Integer> findSubstring(String s, String[] words) {
-        if(s.isEmpty()) return Collections.emptyList();
-        if(words.length <= 0) return Collections.emptyList();
+        if (s.isEmpty()) return Collections.emptyList();
+        if (words.length <= 0) return Collections.emptyList();
         int position = 0;
         int sLen = s.length();
-        int step = words[0].length();
+        int step = words.length * words[0].length();
         String tmp;
-        while(position < sLen){
-            tmp = s.substring(position, step);
+        while (position + step < sLen) {
+            tmp = s.substring(position, position + step);
 
-            position = position + step;
+            position++;
+        }
+    }
+
+    private List<String> getAllStrings(String[] words){
+        List<String> list = new ArrayList<>();
+
+        return list;
+    }
+
+    private void buildStrings(String[] words, Stack<String> stack, int position, List<String> list){
+        if(stack.size() == words.length){
+//            list.add()
+        }else{
+
+
+            if(position < words.length){
+                stack.push(words[position]);
+                buildStrings(words, stack, position+1, list);
+            }else{
+
+            }
         }
     }
 
