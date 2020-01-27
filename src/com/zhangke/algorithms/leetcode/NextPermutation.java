42fab8ac4865a45045ec9761c9a7073fb9c0115f
ZhangKe <zhangke@qingting.fm>
2020-01-27 10:59:28 +0800

    添加下一个排列

--- src/com/zhangke/algorithms/leetcode/NextPermutation.java
+++ src/com/zhangke/algorithms/leetcode/NextPermutation.java
@@ -3,7 +3,6 @@ package com.zhangke.algorithms.leetcode;
 import com.zhangke.algorithms.Util;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 /**
@@ -15,53 +14,50 @@ public class NextPermutation {
 
     public void nextPermutation(int[] nums) {
         if (nums == null || nums.length <= 0) return;
-        List<Integer> numList = new ArrayList<>();
-        getAllNum(nums, new ArrayList<>(), new ArrayList<>(), numList);
-        int originalValue = numList.get(0);
-        Collections.sort(numList);
-        Integer target = null;
-        for (int item : numList) {
-            if (item > originalValue) {
-                target = item;
-                break;
+        int len = nums.length;
+        int right = len - 1;
+        int left = -1;
+        int end = right;
+        int start = 0;
+        while (end > 0) {
+            start = end - 1;
+            while (start >= 0 && nums[start] >= nums[end]) start--;
+            if (start > left) {
+                left = start;
+                right = end;
             }
+            end--;
         }
-        if (target == null) {
-            target = numList.get(0);
-        }
-        int anchor = 10;
-        int sum = 0;
-        for (int i = nums.length - 1; i >= 0; i--) {
-            sum = (target - sum) % anchor;
-            nums[i] = sum / (anchor / 10);
-            anchor = anchor * 10;
+        if (left >= 0) {
+            //将 right 值放入 left，left 之后的值升序排列
+            int rightValue = nums[right], position = right;
+            while (position >= left + 1 && position <= right) {
+                nums[position] = nums[position - 1];
+                position--;
+            }
+            sortNums(nums, left + 1, nums.length);
+            nums[left] = rightValue;
+        } else {
+            //返回升序排列
+            sortNums(nums, 0, nums.length);
         }
     }
 
-    private void getAllNum(int[] data, List<Integer> recordList, List<Integer> positionList, List<Integer> list) {
-        if (recordList.size() == data.length) {
-            StringBuilder builder = new StringBuilder();
-            for (int item : recordList) {
-                builder.append(item);
-            }
-            list.add(Integer.valueOf(builder.toString()));
-        } else {
-            for (int i = 0; i < data.length; i++) {
-                if (!positionList.contains(i)) {
-                    positionList.add(i);
-                    recordList.add(data[i]);
-                    getAllNum(data, recordList, positionList, list);
-                    positionList.remove(Integer.valueOf(i));
-                    if (!recordList.isEmpty()) {
-                        recordList.remove(recordList.size() - 1);
-                    }
-                }
-            }
+    private void sortNums(int[] nums, int start, int end) {
+        List<Integer> sortList = new ArrayList<>(end - start);
+        for (int i = start; i < end; i++) {
+            sortList.add(nums[i]);
+        }
+        sortList.sort((o1, o2) -> o2 - o1);
+        for (int i = start; i < end; i++) {
+            nums[i] = sortList.get(end - i - 1);
         }
     }
 
     public static void main(String[] args) {
-        int[] nums = {2,1,3,1,2,0,4,1,2};
+//        int[] nums = {2,0,2,3,2,0};
+//        int[] nums = {9,5,3,2,1};
+        int[] nums = {1, 3, 2};
         NextPermutation nextPermutation = new NextPermutation();
         nextPermutation.nextPermutation(nums);
         Util.printArray(nums);
