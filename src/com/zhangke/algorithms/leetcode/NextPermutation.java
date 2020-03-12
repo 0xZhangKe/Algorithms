2543b7eee9a5a3f07482dac8e71ab6f818ee5013
ZhangKe <zhangke@qingting.fm>
2020-03-12 09:32:55 +0800

    添加 下一个排列

--- src/com/zhangke/algorithms/leetcode/NextPermutation.java
+++ src/com/zhangke/algorithms/leetcode/NextPermutation.java
@@ -3,7 +3,6 @@ package com.zhangke.algorithms.leetcode;
 import com.zhangke.algorithms.Util;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 /**
@@ -15,55 +14,32 @@ public class NextPermutation {
 
     public void nextPermutation(int[] nums) {
         if (nums == null || nums.length <= 0) return;
-        List<Integer> numList = new ArrayList<>();
-        getAllNum(nums, new ArrayList<>(), new ArrayList<>(), numList);
-        int originalValue = numList.get(0);
-        Collections.sort(numList);
-        Integer target = null;
-        for (int item : numList) {
-            if (item > originalValue) {
-                target = item;
+        int len = nums.length;
+        List<Integer> sortList = new ArrayList<>(len);
+        for (int item : nums) {
+            sortList.add(item);
+        }
+        sortList.sort((o1, o2) -> o2 - o1);
+        boolean isMax = true;
+        for (int i = 0; i < len; i++) {
+            if (i != sortList.indexOf(nums[i])) {
+                isMax = false;
                 break;
             }
         }
-        if (target == null) {
-            target = numList.get(0);
-        }
-        int anchor = 10;
-        int sum = 0;
-        for (int i = nums.length - 1; i >= 0; i--) {
-            sum = (target - sum) % anchor;
-            nums[i] = sum / (anchor / 10);
-            anchor = anchor * 10;
-        }
-    }
-
-    private void getAllNum(int[] data, List<Integer> recordList, List<Integer> positionList, List<Integer> list) {
-        if (recordList.size() == data.length) {
-            StringBuilder builder = new StringBuilder();
-            for (int item : recordList) {
-                builder.append(item);
-            }
-            list.add(Integer.valueOf(builder.toString()));
-        } else {
-            for (int i = 0; i < data.length; i++) {
-                if (!positionList.contains(i)) {
-                    positionList.add(i);
-                    recordList.add(data[i]);
-                    getAllNum(data, recordList, positionList, list);
-                    positionList.remove(Integer.valueOf(i));
-                    if (!recordList.isEmpty()) {
-                        recordList.remove(recordList.size() - 1);
-                    }
-                }
+        if (isMax) {
+            for (int i = 0; i < len; i++) {
+                nums[i] = sortList.get(len - i - 1);
             }
         }
     }
 
     public static void main(String[] args) {
-        int[] nums = {2,1,3,1,2,0,4,1,2};
+        long start = System.currentTimeMillis();
+        int[] nums = {1, 4, 2, 3, 1};
         NextPermutation nextPermutation = new NextPermutation();
         nextPermutation.nextPermutation(nums);
         Util.printArray(nums);
+        System.out.println("spend:" + (System.currentTimeMillis() - start));
     }
 }
