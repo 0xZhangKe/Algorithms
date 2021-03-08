3a9fca165a4cc5b4af381e8bdb373375befb20b1
ZhangKe <zhangke@qingting.fm>
2021-03-08 13:45:16 +0800

    完成循环链表

--- src/main/java/com/zhangke/algorithms/Main.java
+++ src/main/java/com/zhangke/algorithms/Main.java
@@ -1,7 +1,5 @@
 package com.zhangke.algorithms;
 
-import io.reactivex.rxjava3.core.Observable;
-
 public class Main {
 
     public static void main(String[] args) {
--- src/main/java/com/zhangke/algorithms/data/ListNode.java
+++ src/main/java/com/zhangke/algorithms/data/ListNode.java
@@ -1,5 +1,8 @@
 package com.zhangke.algorithms.data;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Created by ZhangKe on 2019/10/31.
  */
@@ -10,5 +13,28 @@ public class ListNode {
 
     public ListNode(int x) {
         val = x;
+    }
+
+    public ListNode(int x, ListNode next) {
+        val = x;
+        this.next = next;
+    }
+
+    public String getDesc() {
+        StringBuilder builder = new StringBuilder();
+        ListNode curNode = this;
+        Set<ListNode> nodeSet = new HashSet<>();
+        while (curNode != null) {
+            if (nodeSet.contains(curNode)) {
+                break;
+            }
+            nodeSet.add(curNode);
+            builder.append(curNode.val);
+            curNode = curNode.next;
+            if (curNode != null) {
+                builder.append("->");
+            }
+        }
+        return builder.toString();
     }
 }
