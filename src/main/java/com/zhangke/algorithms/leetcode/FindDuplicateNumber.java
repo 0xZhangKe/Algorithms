75bae596983d0fba78bc5857050c631311479357
ZhangKe <zhangke@qingting.fm>
2021-03-09 14:20:43 +0800

    完成寻找重复数

--- src/main/java/com/zhangke/algorithms/leetcode/FindDuplicateNumber.java
+++ src/main/java/com/zhangke/algorithms/leetcode/FindDuplicateNumber.java
@@ -8,12 +8,62 @@ package com.zhangke.algorithms.leetcode;
 public class FindDuplicateNumber {
 
     public int findDuplicate(int[] nums) {
-        return 0;
+        int fast = 0;
+        int slow = 0;
+        do {
+            fast = nums[nums[fast]];
+            slow = nums[slow];
+        } while (fast != slow);
+        fast = 0;
+        do {
+            fast = nums[fast];
+            slow = nums[slow];
+        } while (fast != slow);
+        return slow;
     }
 
     public static void main(String[] args) {
+        case1();
+        case2();
+        case3();
+        case4();
+        case5();
+        case6();
+    }
+
+    private static void case1() {
         FindDuplicateNumber fdn = new FindDuplicateNumber();
         int[] nums = {1, 3, 4, 2, 2};
+        System.out.println(fdn.findDuplicate(nums));
+    }
+
+    private static void case2() {
+        FindDuplicateNumber fdn = new FindDuplicateNumber();
+        int[] nums = {3, 1, 3, 4, 2};
+        System.out.println(fdn.findDuplicate(nums));
+    }
+
+    private static void case3() {
+        FindDuplicateNumber fdn = new FindDuplicateNumber();
+        int[] nums = {1, 1};
+        System.out.println(fdn.findDuplicate(nums));
+    }
+
+    private static void case4() {
+        FindDuplicateNumber fdn = new FindDuplicateNumber();
+        int[] nums = {1, 1, 2};
+        System.out.println(fdn.findDuplicate(nums));
+    }
+
+    private static void case5() {
+        FindDuplicateNumber fdn = new FindDuplicateNumber();
+        int[] nums = {1, 3, 4, 2, 1};
+        System.out.println(fdn.findDuplicate(nums));
+    }
+
+    private static void case6() {
+        FindDuplicateNumber fdn = new FindDuplicateNumber();
+        int[] nums = {4, 3, 1, 4, 2};
         System.out.println(fdn.findDuplicate(nums));
     }
 }
