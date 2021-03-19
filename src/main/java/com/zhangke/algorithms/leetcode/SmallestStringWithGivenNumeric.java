0d33a5aec35e8f2bc2883210daa5cda2a22d1ca8
ZhangKe <zhangke@qingting.fm>
2021-03-19 19:10:09 +0800

    完善 二维数组中的查找

---
+++ src/main/java/com/zhangke/algorithms/leetcode/SmallestStringWithGivenNumeric.java
@@ -0,0 +1,22 @@
+package com.zhangke.algorithms.leetcode;
+
+/**
+ * 1663. 具有给定数值的最小字符串
+ * https://leetcode-cn.com/problems/smallest-string-with-a-given-numeric-value/
+ * Created by ZhangKe on 2021/3/17.
+ */
+public class SmallestStringWithGivenNumeric {
+
+    public String getSmallestString(int n, int k) {
+        return "";
+    }
+
+    public static void main(String[] args){
+        case1();
+    }
+
+    private static void case1(){
+        SmallestStringWithGivenNumeric s = new SmallestStringWithGivenNumeric();
+
+    }
+}
