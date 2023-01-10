df3d136d58a158244dcae6f720cb0fcf6a59770d
ZhangKe <kezhang404@gmail.com>
2023-01-10 22:18:57 +0800

    Add comment

--- src/main/java/com/zhangke/algorithms/leetcode/GroupAnagrams.java
+++ src/main/java/com/zhangke/algorithms/leetcode/GroupAnagrams.java
@@ -7,6 +7,8 @@ import java.util.*;
 /**
  * 49. 字母异位词分组
  * <a href="https://leetcode.cn/problems/group-anagrams/submissions/">49. 字母异位词分组</a>
+ * 首先设计一个算法，输入相同的 字母异位词分组 但不同的字符串输出相同的值，这里通过最字符串按照char顺序排序，然后输出排序后的字符串来实现。
+ * 然后创建一个 key 为上述算法输出值，value 为 list 的数据结构，然后遍历输入列表并将item存入 list，最终这个 Map 的 value 就是结果。
  */
 public class GroupAnagrams {
 
